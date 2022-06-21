580335bcde17a9d55397f9d022bd1070b30f6f16
ijul <ijuldz@gmail.com>
2022-06-22 01:07:02 +0700

    add api_token auth migration

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -6,6 +6,9 @@ export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number;
 
+  @column()
+  public email: string;
+
   @belongsTo(() => Role)
   public role: BelongsTo<typeof Role>;
 
