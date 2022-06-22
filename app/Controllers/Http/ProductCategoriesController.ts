d091aacc132f5b98f52b5a230fd4c40fbfe8adbd
ijul <ijuldz@gmail.com>
2022-06-22 22:07:33 +0700

    product category create

--- app/Controllers/Http/ProductCategoriesController.ts
+++ app/Controllers/Http/ProductCategoriesController.ts
@@ -1,7 +1,40 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Application from "@ioc:Adonis/Core/Application";
+
+import ProductCategory from "App/Models/ProductCategory";
 
 export default class ProductCategoriesController {
   public async index(ctx: HttpContextContract) {
     return "bisa";
+  }
+
+  public async create({ request, response }) {
+    const name = request.input("name");
+
+    const image = request.file("image", {
+      size: "2mb",
+      extnames: ["jpg", "png", "gif"],
+    });
+
+    if (!image) {
+      return;
+    }
+
+    if (!image.isValid) {
+      return image.errors;
+    }
+
+    await image.moveToDisk("./product-categories");
+
+    const filename = image.fileName;
+
+    const category = await ProductCategory.create({
+      name,
+      image: `product-categories/${filename}`,
+    });
+
+    response.json({
+      data: category,
+    });
   }
 }
